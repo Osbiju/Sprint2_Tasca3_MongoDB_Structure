922136fc00206faabc568ac218bb2c7163bfd66d
Osbiju <127254695+Osbiju@users.noreply.github.com>
2024-01-21 18:16:00 +0100

    Update Glasses.js

--- Tasca3/n1e2/Glasses.js
+++ Tasca3/n1e2/Glasses.js
@@ -1,4 +1,5 @@
 
+
 db.createCollection( 'Glasses', {validator: {$jsonSchema: {bsonType: 'object',title:'Glasses',required: [         'Brand',          'Provider',          'Price',          'Graduation',          'Frame_type',          'Frame_colour',          'Glass_colour',          'Sale'],properties: {Brand: {bsonType: 'string'},Provider: {bsonType: 'object',
 title:'object',required: [         'Name',          'Adress',          'PhoneNumber',          'Fax',          'NIF'],properties: {Name: {bsonType: 'string'},Adress: {bsonType: 'object',
 title:'object',required: [         'Street',          'Num',          'Floor',          'Door',          'City',          'Country',          'PostalCode'],properties: {Street: {bsonType: 'string'},Num: {bsonType: 'int'},Floor: {bsonType: 'int'},Door: {bsonType: 'int'},City: {bsonType: 'string'},Country: {bsonType: 'string'},PostalCode: {bsonType: 'int'}}},PhoneNumber: {bsonType: 'int'},Fax: {bsonType: 'int'},NIF: {bsonType: 'string'}}},Price: {bsonType: 'double'},Graduation: {bsonType: 'object',
